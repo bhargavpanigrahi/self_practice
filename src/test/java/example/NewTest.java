0e1a0d3be573799afe802a7c116b715a6fbb284f
bhargavpanigrahi <p.bhargav1991@gmail.com>
2018-04-18 06:27:31 -0400

    this is my first commit

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,34 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	
+	private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	@BeforeTest
+	public void beforeClass() {
+		   
+		  System.setProperty("webdriver.ie.driver",
+
+					"C:\\Users\\BHPANI\\Downloads\\IEDriverServer_Win32_2.48.0\\IEDriverServer.exe");
+	   driver = new InternetExplorerDriver();
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}		
+
+}
