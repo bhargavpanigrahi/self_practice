10feb1434101247d44ca96ffb9f9b86a8a83f641
bhargavpanigrahi <p.bhargav1991@gmail.com>
2018-04-18 08:37:13 -0400

    new file added

---
+++ src/test/java/example/Gitdumps.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Gitdumps {
+
+	public WebDriver driver;
+	 @Test
+	  public void openMyBlog() {
+	 driver.get("https://www.softwaretestingmaterial.com/");
+	  }
+}
