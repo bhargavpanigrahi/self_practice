03a9384d6ad2f4b85ea9bdb74055cfeef2d61df9
bhargavpanigrahi <p.bhargav1991@gmail.com>
2018-04-18 07:32:26 -0400

    this was an intentional commit

---
+++ src/test/java/example/Testing.java
@@ -0,0 +1,33 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Testing {
+	
+	public WebDriver driver;
+	 @Test
+	  public void openMyBlog() {
+	 driver.get("https://www.softwaretestingmaterial.com/");
+	  }
+	  
+	  @BeforeClass
+	  public void beforeClass() {
+	   
+		  System.setProperty("webdriver.ie.driver",
+
+					"C:\\Users\\BHPANI\\Downloads\\IEDriverServer_Win32_2.48.0\\IEDriverServer.exe");
+	   driver = new InternetExplorerDriver();
+	   
+	  }
+	 
+	  @AfterClass
+	  public void afterClass() {
+		  System.out.println("kill the browser");
+	   driver.quit();
+	  }
+
+}
